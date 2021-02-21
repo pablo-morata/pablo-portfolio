3715735f2ec6bccae11fe957e7ea33f149f26bea
Yang Ge <holayangge@gmail.com>
2021-02-21 17:58:58 +0100

    Updates

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('.full-lengh-portraits').click(function(){
+    $('.toggle').removeClass('show');
+    $('.full-lengh-portraits-imgs').addClass('show');
+})
+$('.portraits').click(function(){
+    $('.toggle').removeClass('show');
+    $('.portraits-imgs').addClass('show');
+})
+$('.contact').click(function(){
+    $('.toggle').removeClass('show');
+    $('.contact-info').addClass('show');
+})
+
